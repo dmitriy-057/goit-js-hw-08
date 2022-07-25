f390aca0f6d8970c0ea0c1ff7437e689ca9ca8db
Dmitriy Gonharov <my.webpage057@gmail.com>
2022-07-26 00:44:52 +0300

    add 2st task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector("#vimeo-player");
+const player = new Player(iframe);
+
+
+player.on('timeupdate', throttle(onCurrentTime, 1000));
+
+onSaveCurrentTime()
+
+function onCurrentTime(currentTime) {
+
+console.log("seconds:", currentTime.seconds);
+localStorage.setItem("videoplayer-current-time",JSON.stringify(currentTime.seconds))
+}
+
+function onSaveCurrentTime() {
+    const savedCurrentTime = localStorage.getItem("videoplayer-current-time");
+    const parsedCurrentTime = JSON.parse(savedCurrentTime);
+    console.log("видео было остановвлено на:",parsedCurrentTime);
+    player.setCurrentTime(parsedCurrentTime)
+}
