4f94e47a1c6a9f93d18041f22337792709461d1b
Dmitriy Gonharov <my.webpage057@gmail.com>
2022-07-27 01:02:10 +0300

    add 3st task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle'
+const STORAGE_KEY = "feedback-form-state";
+
+let formData = {
+  email: '',
+  message: '',
+};
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    input: document.querySelector('input'),
+    textArea: document.querySelector('textarea'),
+  };
+  refs.form.addEventListener('input', throttle(onFormData, 500));
+  refs.form.addEventListener('submit', onFormSubmit);
+  savedFormData();
+
+// cобытие для отправки формы
+function onFormSubmit(e) {
+    e.preventDefault();
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    console.log(savedMessage);
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  }
+
+
+  function onFormData(e) {
+    formData[e.target.name] = e.target.value;
+    console.log(formData);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }
+  function savedFormData() {
+    const savedValue = localStorage.getItem(STORAGE_KEY);
+    if (!savedValue) {
+      return;
+    }
+    const { email, message } = JSON.parse(savedValue);
+    formData = { email, message };
+    console.log('formData', formData);
+    refs.input.value = email;
+    refs.textArea.value = message;
+  }
+
+
 
